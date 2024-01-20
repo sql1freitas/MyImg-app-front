629ad212890bd411077f06214063a8b197d31b1e
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-20 07:58:57 -0300

    Download transferido para click na imagem

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ImageCardProps {
     nome?: string;
     tamanho?: number;
@@ -8,9 +10,14 @@ interface ImageCardProps {
 
 
 export const ImageCard: React.FC<ImageCardProps> = ({nome, tamanho, dataUpload, src}) => {
+    
+    function download(){
+        window.open(src, '_blank')
+    }
+    
     return(
         <div className="card relative bg-white rounded-md shadow-md transition-transform ease in duration-300 transform hover:shadow-lg hover:-translate-y-2">
-                <img src= {src}  className="h-56 w-full object-cover rounded-t-md" alt="" />
+                <img src= {src}  onClick={download} className="h-56 w-full object-cover rounded-t-md" alt="" />
                 <div className="card-body p-4">
                     <h5 className="text-xl font-semibold mb-2 text-gray-600"> {nome} </h5>
                     <p className="text-gray-600"> {tamanho} </p>
